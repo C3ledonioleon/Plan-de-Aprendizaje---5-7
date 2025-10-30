8800f431bdb3f9a8beab5c582f620d6d2e923213
C3ledonioleon <celedonioleon3@gmail.com>
2025-10-30 12:03:36 -0300

    Agregando Organizador en Usuario.sql
    Introduces a new 'Organizador' user with specific SELECT, INSERT, and UPDATE privileges on several 5to_SVE tables. Also adds FLUSH PRIVILEGES at the end of the script to apply changes.

--- Scripts/Usuario.sql
+++ Scripts/Usuario.sql
@@ -1,5 +1,6 @@
 CREATE USER IF NOT EXISTS 'Cliente'@'%' IDENTIFIED BY 'PassCl1et@123';
 CREATE USER IF NOT EXISTS 'Administrador'@'localhost' IDENTIFIED BY 'Admin!2345';
+CREATE USER IF NO EXISTS 'Organizador'@'%' IDENTIFIED BY '0rg@niZador#1';
 CREATE USER IF NOT EXISTS 'Molinete'@'10.160.2%' IDENTIFIED BY 'M0linete@987';
 
 -- CLIENTE 
@@ -9,6 +10,16 @@ GRANT SELECT ON SVE.Entrada TO 'Cliente'@'%';
 GRANT SELECT ON SVE.Funcion TO 'Cliente'@'%';
 GRANT SELECT ON SVE.Evento TO 'Cliente'@'%';
 
+
+-- ORGANIZADOR
+GRANT SELECT, INSERT, UPDATE ON 5to_SVE.Evento TO 'Organizador'@'%';
+GRANT SELECT, INSERT, UPDATE ON 5to_SVE.Funcion TO 'Organizador'@'%';
+GRANT SELECT, INSERT, UPDATE ON 5to_SVE.Tarifa TO 'Organizador'@'%';
+GRANT SELECT, INSERT, UPDATE ON 5to_SVE.Entrada TO 'Organizador'@'%';
+GRANT SELECT, INSERT, UPDATE ON 5to_SVE.Orden TO 'Organizador'@'%';
+GRANT SELECT ON 5to_SVE.Local TO 'Organizador'@'%';
+GRANT SELECT ON 5to_SVE.Sector TO 'Organizador'@'%';
+
 -- ADMIN
 GRANT ALL ON SVE.* TO 'Administrador'@'localhost';
 
@@ -16,5 +27,8 @@ GRANT ALL ON SVE.* TO 'Administrador'@'localhost';
 GRANT SELECT ON SVE.Cliente TO 'Molinete'@'10.3.45.%';
 GRANT SELECT ON SVE.Funcion TO 'Molinete'@'10.3.45.%';
 GRANT SELECT, INSERT, UPDATE ON SVE.Entrada TO 'Molinete'@'10.3.45.%';	
+
+FLUSH PRIVILEGES;
+
 
 

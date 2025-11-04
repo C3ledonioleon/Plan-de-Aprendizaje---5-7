ef4a070a76ced7b7a8c162c13a709b9986a944b6
C3ledonioleon <celedonioleon3@gmail.com>
2025-11-04 12:10:51 -0300

    Cambios en el Usuario.sql

--- Scripts/Usuario.sql
+++ Scripts/Usuario.sql
@@ -1,17 +1,15 @@
-CREATE USER IF NOT EXISTS 'Cliente'@'%' IDENTIFIED BY 'PassCl1et@123';
-CREATE USER IF NOT EXISTS 'Administrador'@'localhost' IDENTIFIED BY 'Admin!2345';
-CREATE USER IF NOT EXISTS 'Organizador'@'%' IDENTIFIED BY '0rg@niZador#1';
-CREATE USER IF NOT EXISTS 'Molinete'@'10.120.0%' IDENTIFIED BY 'M0linete@987';
 
 -- CLIENTE 
-GRANT SELECT, UPDATE ON SVE.Usuario TO 'Cliente'@'%';
-GRANT SELECT ON SVE.Cliente TO 'Cliente'@'%';
-GRANT SELECT ON SVE.Entrada TO 'Cliente'@'%';
-GRANT SELECT ON SVE.Funcion TO 'Cliente'@'%';
-GRANT SELECT ON SVE.Evento TO 'Cliente'@'%';
+CREATE USER IF NOT EXISTS 'Cliente'@'%' IDENTIFIED BY 'PassCl1et@123';
+GRANT SELECT, UPDATE ON 5to_SVE.Usuario TO 'Cliente'@'%';
+GRANT SELECT ON 5to_SVE.Cliente TO 'Cliente'@'%';
+GRANT SELECT ON 5to_SVE.Entrada TO 'Cliente'@'%';
+GRANT SELECT ON 5to_SVE.Funcion TO 'Cliente'@'%';
+GRANT SELECT ON 5to_SVE.Evento TO 'Cliente'@'%';
 
 
 -- ORGANIZADOR
+CREATE USER IF NOT EXISTS 'Organizador'@'%' IDENTIFIED BY '0rg@niZador#1';
 GRANT SELECT, INSERT, UPDATE ON 5to_SVE.Evento TO 'Organizador'@'%';
 GRANT SELECT, INSERT, UPDATE ON 5to_SVE.Funcion TO 'Organizador'@'%';
 GRANT SELECT, INSERT, UPDATE ON 5to_SVE.Tarifa TO 'Organizador'@'%';
@@ -21,14 +19,14 @@ GRANT SELECT ON 5to_SVE.Local TO 'Organizador'@'%';
 GRANT SELECT ON 5to_SVE.Sector TO 'Organizador'@'%';
 
 -- ADMIN
-GRANT ALL ON SVE.* TO 'Administrador'@'localhost';
+CREATE USER IF NOT EXISTS 'Administrador'@'localhost' IDENTIFIED BY 'Admin!2345';
+GRANT ALL ON 5to_SVE.* TO 'Administrador'@'localhost';
 
 -- MOLINETE
-GRANT SELECT ON SVE.Cliente TO 'Molinete'@'10.120.0%';
-GRANT SELECT ON SVE.Funcion TO 'Molinete'@'10.120.0%';
-GRANT SELECT, INSERT, UPDATE ON SVE.Entrada TO 'Molinete'@'10.120.0%';	
+CREATE USER IF NOT EXISTS 'Molinete'@'10.120.0%' IDENTIFIED BY 'M0linete@987';
+GRANT SELECT ON 5to_SVE.Cliente TO 'Molinete'@'10.120.0%';
+GRANT SELECT ON 5to_SVE.Funcion TO 'Molinete'@'10.120.0%';
+GRANT SELECT, INSERT, UPDATE ON 5to_SVE.Entrada TO 'Molinete'@'10.120.0%';	
 
 FLUSH PRIVILEGES;
-
-
 
